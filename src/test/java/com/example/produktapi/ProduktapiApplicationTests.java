e5ba6392159a6bf446a0f754c49b2049ef520d9f
Simon <simon.andersson@sprinto.se>
2023-03-03 10:28:02 +0100

    Added failing test

--- src/test/java/com/example/produktapi/ProduktapiApplicationTests.java
+++ src/test/java/com/example/produktapi/ProduktapiApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.produktapi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class ProduktapiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(3, 5);
 	}
 
 }
